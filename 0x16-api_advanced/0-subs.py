74509d5f61241957323e43ac77e7173885b73289
joan cruz <joancruz95@gmail.com>
2020-02-07 02:51:06 +0000

    Add comment

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,6 +8,7 @@ import sys
 
 
 def number_of_subscribers(subreddit):
+    '''Return number of subscribers for subreddit'''
     r = get('https://www.reddit.com/r/{}/about.json'.format(subreddit),
             headers={"User-agent": "joancruz"}, allow_redirects=False)
     if r.status_code is not 200:
