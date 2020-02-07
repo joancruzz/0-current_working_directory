be84bb282610ed069d321554e0ae6b1bd043d502
joan cruz <joancruz95@gmail.com>
2020-02-07 02:55:27 +0000

    Remove imports

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,15 +3,13 @@
 # and returns the number of subscribers
 
 from requests import get
-import json
-import sys
 
 
 def number_of_subscribers(subreddit):
     '''Return number of subscribers for subreddit'''
     r = get('https://www.reddit.com/r/{}/about.json'.format(subreddit),
             headers={"User-agent": "joancruz"}, allow_redirects=False)
-    if r.status_code is not 200:
+    if r.status_code != 200:
         return 0
     else:
         return r.json().get('data').get('subscribers')
