a07e5af0148bb66a784503b8082dbf76a4b69acb
joan cruz <joancruz95@gmail.com>
2020-02-07 02:28:46 +0000

    Function that queries the Reddit API and returns the number of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# Function that queries the Reddit API
+# and returns the number of subscribers
+
+from requests import get
+import json
+import sys
+
+
+def number_of_subscribers(subreddit):
+    r = get('https://www.reddit.com/r/{}/about.json'.format(subreddit),
+            headers={"User-agent": "joancruz"}, allow_redirects=False)
+    subs = r.json().get('data').get('subscribers')
+    if r.status_code is not 200:
+        return 0
+    else:
+        return subs
