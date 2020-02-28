e92f23d3231d5959912fc69daf47c20d832912f3
joancruzz <51678245+joancruzz@users.noreply.github.com>
2020-02-28 00:08:47 -0800

    Update 0-the_sky_is_the_limit_not.pp
    edit whitespaces

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,17 +1,9 @@
 # change upperimit
-
 exec { 'upper limit':
-
   command => 'sed -i s/15/1000/ /etc/default/nginx',
-
   path    => '/bin',
-
 }
-
 service { 'nginx':
-
     ensure    => running,
-
     subscribe => Exec['upper limit'],
-
 }
