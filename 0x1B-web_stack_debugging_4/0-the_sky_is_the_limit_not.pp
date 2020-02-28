28f1f4218b3c88125302ac403157a9ab3aab861b
joan cruz <joancruz95@gmail.com>
2020-02-28 08:21:08 +0000

    Edit comments

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,9 @@
-# change upperimit
+# change upperlimit
 exec { 'upper limit':
-  command => 'sed -i s/15/1000/ /etc/default/nginx',
-  path    => '/bin',
+  command => 'sed -i s/15/1000/ /etc/default/nginx',
+  path    => '/bin',
 }
 service { 'nginx':
-    ensure    => running,
-    subscribe => Exec['upper limit'],
+    ensure    => running,
+    subscribe => Exec['upper limit'],
 }
--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,17 +1,9 @@
 # set userlimits
-
 exec { 'hard limit':
-
-  command => 'sed -i "s/holberton hard nofile 5/holberton hard nofile 1000/" /etc/security/limits.conf',
-
-  path    => '/bin',
-
+  command => 'sed -i "s/holberton hard nofile 5/holberton hard nofile 1000/" /etc/security/limits.conf',
+  path    => '/bin',
 }
-
 exec { 'soft limit':
-
-  command => 'sed -i "s/holberton soft nofile 4/holberton soft nofile 1000/" /etc/security/limits.conf',
-
-  path    => '/bin',
-
+  command => 'sed -i "s/holberton soft nofile 4/holberton soft nofile 1000/" /etc/security/limits.conf',
+  path    => '/bin',
 }
